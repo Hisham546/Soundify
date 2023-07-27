7e0d6efce0f9723a118fd780d98299bc3998443f
Hisham <hmuhammed546@gmail.com>
2023-07-27 11:00:34 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Dashboard from './src/home/dashboard';
+import PlaySong from './src/home/playsong';
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator>
+           <Stack.Screen
+              options={{headerShown : false}}
+              name="Dashboard"
+              component={Dashboard} />
+          <Stack.Screen
+                 options={{headerShown : false}}
+                 name="PlaySong"
+                 component={Dashboard} />
+              </Stack.Navigator>
+     </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ src/home/dashboard.js
@@ -0,0 +1,41 @@
+import React, { useEffect,useRef,useState } from "react";
+
+import {
+View,
+Image,
+Text,Button,
+StyleSheet,TouchableOpacity,FlatList,
+TextInput}
+from "react-native";
+import {widthPercentageToDP as wp,heightPercentageToDP as hp} from 'react-native-responsive-screen'
+import TrackPlayer from 'react-native-track-player';
+export default function Dashboard({}){
+
+
+
+
+
+
+
+
+
+
+return(
+
+<View><Text>Music Player</Text></View>
+
+
+
+);
+
+
+}
+
+const styles = StyleSheet.create({
+
+
+
+
+
+
+})
