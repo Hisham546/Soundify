7c268df4a7c1e53d4364947c086d3f339c9d8f88
Hisham <hmuhammed546@gmail.com>
2023-08-06 21:47:21 +0530

    setted up  vector icon packages

--- src/home/playsong.js
+++ src/home/playsong.js
@@ -1,15 +1,16 @@
-import React, { useEffect,useRef,useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import {
-View,
-Image,
-Text,Button,
-StyleSheet,TouchableOpacity,FlatList,
-TextInput}
-from "react-native";
-import {widthPercentageToDP as wp,heightPercentageToDP as hp} from 'react-native-responsive-screen'
+    View,
+    Image,
+    Text, Button,
+    StyleSheet, TouchableOpacity, FlatList,
+    TextInput
+}
+    from "react-native";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 import TrackPlayer from 'react-native-track-player';
-export default function PlaySong({}){
+export default function PlaySong({ }) {
 
 
 
@@ -20,48 +21,48 @@ export default function PlaySong({}){
 
 
 
-return(
+    return (
 
-<View style ={styles.mainContainer}>
+        <View style={styles.mainContainer}>
 
-<View style={styles.firstView}>
-
-
-</View>
-
-<View style={styles.secondView}>
+            <View style={styles.firstView}>
 
 
-</View>
+            </View>
 
-</View>
+            <View style={styles.secondView}>
+
+
+            </View>
+
+        </View>
 
 
 
-);
+    );
 
 
 }
 
 const styles = StyleSheet.create({
 
-mainContainer:{
-    flex:1,
-    backgroundColor:'white'
+    mainContainer: {
+        flex: 1,
+        backgroundColor: 'white'
 
-},
-firstView:{
-width:wp('100'),
-height:hp('50'),
-
-
-},
-secondView:{
-    width:wp('100'),
-    height:hp('50'),
+    },
+    firstView: {
+        width: wp('100'),
+        height: hp('60'),
 
 
-}
+    },
+    secondView: {
+        width: wp('100'),
+        height: hp('40'),
+
+
+    }
 
 
 
