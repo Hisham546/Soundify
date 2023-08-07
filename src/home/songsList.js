86056a5da695e0d9687e493849b65a966c9e5ef1
Hisham <hmuhammed546@gmail.com>
2023-08-07 20:09:14 +0530

    adding ui in songs list

--- src/home/songsList.js
+++ src/home/songsList.js
@@ -9,45 +9,115 @@ TextInput}
 from "react-native";
 import {widthPercentageToDP as wp,heightPercentageToDP as hp} from 'react-native-responsive-screen'
 import TrackPlayer from 'react-native-track-player';
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Slider from '@react-native-community/slider';
 
 export default function SongsList({}){
 
-    useEffect(() => {
-    start()
-
-    })
-
-
-    const start = async () => {
-        // Set up the player
-        await TrackPlayer.setupPlayer();
-    
-        // Add a track to the queue
-      
-        console.log(TrackPlayer)
-    }
 
 
 
+    return (
+
+        <View style={styles.mainContainer}>
+
+            <View style={styles.firstView}>
+                <View style={styles.topIconView}>
+                    <TouchableOpacity onPress={() => navigation.navigate('SongsList')}>
+                    <MaterialIcon name={'chevron-down'} size={hp('3.5%')} color={'white'} style={{marginLeft:wp('3')}} />
+                    </TouchableOpacity>
+                 
+                </View>
+                <Image
+                    style={styles.sampleLogo}
+                    source={require('../assets/images/Beatles.png')}
+                />
+
+            </View>
+
+            <View style={styles.secondView}>
+                <View style={styles.sliderView}>
+
+                    <Text style={{ marginLeft: wp('5'), color: 'white', marginTop: hp('1'), fontSize: hp('1.70') }}>From Me To You - Mono Remastered</Text>
+                    <View style={{ width: wp('100'), height: hp('5'), flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <Text style={{ marginLeft: wp('5'), color: 'white', fontSize: hp('1.50') }}>Beatless</Text>
+                        <MaterialIcon name={'cards-heart-outline'} size={hp('3%')} color={'white'} style={{ marginRight: wp('4') }} />
+                    </View>
+                    <Slider
+                        style={{ width: wp('98'), height: hp('1') }}
+                        minimumValue={0}
+                        maximumValue={1}
+                        thumbTintColor="white"
+                        minimumTrackTintColor="#FFFFFF"
+                        maximumTrackTintColor="#A8A196"
+                    />
+
+                </View>
+               
+
+            </View>
+
+        </View>
 
 
-return(
 
-<View><Text>list</Text></View>
-
-
-
-);
+    );
 
 
 }
 
-
-
-
 const styles = StyleSheet.create({
 
+    mainContainer: {
+        flex: 1,
+        backgroundColor: '#93261d'
 
+    },
+    firstView: {
+        width: wp('100'),
+        height: hp('65'),
+
+
+    },
+    secondView: {
+        width: wp('100'),
+        height: hp('35'),
+
+
+
+
+    },
+    sliderView: {
+        width: wp('100'),
+        height: hp('10'),
+
+    },
+    sampleLogo: {
+        width: wp('73'),
+        height: hp('33'),
+        marginLeft: wp('11'),
+        marginTop: hp('1')
+
+    },
+    controllerView: {
+        width: wp('100'),
+        height: hp('10'),
+        marginTop: hp('1'),
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center'
+
+    },
+    topIconView: {
+        marginTop:hp('1'),
+        width: wp('100'),
+        height: hp('5'),
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+
+
+
+    }
 
 
 
