d90b78d81f61b478afd75a4e6b98dfabfc93062f
Hisham <hmuhammed546@gmail.com>
2023-08-15 22:25:37 +0530

    added basic recent playlist ui

--- src/home/dashboard.js
+++ src/home/dashboard.js
@@ -5,16 +5,16 @@ import {
   Image,
   Text, Button,
   StyleSheet, TouchableOpacity, FlatList,
-  Dimensions 
+  Dimensions
 }
   from "react-native";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 import TrackPlayer from 'react-native-track-player';
 import DocumentPicker from 'react-native-document-picker'
 import * as RNFS from '@dr.pogodin/react-native-fs';
-
-  const screenWidth = Dimensions.get('window').width;
-  const screenHeight = Dimensions.get('window').height;
+import CardView from 'react-native-cardview'
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
 export default function Dashboard({ navigation }) {
 
 
@@ -37,7 +37,27 @@ export default function Dashboard({ navigation }) {
   //   console.log(TrackPlayer)
   // }
 
+  const songs = [
+    {
+      id: '1',
+      song: 'Love me like you do',
+      title: 'Ellie Goudling',
+      image: require('../assets/images/Beatles.png')
+    },
+    {
+      id: '2',
+      song: 'Beutifull',
+      title: 'Akon',
+      image: require('../assets/images/Beatles.png')
+    },
+    {
+      id: '3',
+      song: 'Dust till dawn',
+      title: 'Zayn',
+      image: require('../assets/images/Beatles.png')
+    },
 
+  ]
 
 
 
@@ -49,7 +69,29 @@ export default function Dashboard({ navigation }) {
 
         <Text style={styles.heading}>Recently Played</Text>
       </View>
+      <View style={styles.scrollableView}>
+        <FlatList
+          horizontal
+          data={songs}
+          style={{ width: wp('100'), height: hp('5'), marginTop: hp('1') }}
+          renderItem={({ item }) =>
+            <CardView
+             style={styles.recentCardView}
+              cornerRadius={5}>
+              <TouchableOpacity onPress={() => navigation.navigate('PlaySong', { songs: item })}
+                style={{ width: wp('100'), height: hp('5') }}>
+                   <Image
+                    style={styles.sampleLogo}
+                    source={item.image}
+                />
+                <Text style={{ marginLeft: wp('8'), color: 'white', fontSize: hp('2') }}>{item.song}</Text>
+              </TouchableOpacity>
+            </CardView>
+          }
+          keyExtractor={item => item.id}
+        />
 
+      </View>
     </View>
 
 
@@ -73,18 +115,30 @@ const styles = StyleSheet.create({
   heading: {
     color: 'white',
     fontSize: screenHeight * 0.02,
-    fontFamily:'Manrope-Bold',
-    marginLeft:wp('2')
+    fontFamily: 'Manrope-Bold',
+    marginLeft: wp('2')
   },
   topIconView: {
     marginTop: hp('1'),
     width: screenWidth * 100,
     height: screenHeight * 0.05,
     flexDirection: 'row',
-    alignItems:'center',
+    alignItems: 'center',
     justifyContent: 'space-between',
 
   },
+  scrollableView: {
+    width: screenWidth * 100,
+    height: screenHeight * 0.19,
+
+
+  },
+  recentCardView:{
+  width:wp('40'),
+  height:hp('17'),
+  marginLeft:wp('1')
+
+  }
 
 
 
